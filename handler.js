c2fd12f2e914d45c94af6ce795c4b2007eb2d208
sendiany <septiantos@outlook.com>
2022-06-22 23:19:58 +0700

    finish making api

--- handler.js
+++ handler.js
@@ -1,16 +1,17 @@
 import { nanoid } from 'nanoid'
 import books from './books.js'
+
 const addBookHandler = (request, h) => {
   const { name, year, author, summary, publisher, pageCount, readPage, reading } = request.payload
 
   const id = nanoid(16)
-  const insertAt = new Date().toISOString()
-  const updatedAt = insertAt
-  const finished = pageCount === readPage
+  const insertedAt = new Date().toISOString()
+  const updatedAt = insertedAt
+  const finished = readPage === pageCount
   const newBooks = {
-    name, year, author, summary, publisher, pageCount, readPage, reading, id, finished, insertAt, updatedAt
+    name, year, author, summary, publisher, pageCount, readPage, reading, id, finished, insertedAt, updatedAt
   }
-  if (name === '') {
+  if (!name) {
     const response = h.response({
       status: 'fail',
       message: 'Gagal menambahkan buku. Mohon isi nama buku'
@@ -26,8 +27,8 @@ const addBookHandler = (request, h) => {
     response.code(400)
     return response
   }
-
-  const isSuccess = books.filter((note) => note.id === id).length > 0
+  books.push(newBooks)
+  const isSuccess = books.filter((b) => b.id === id).length > 0
   if (isSuccess) {
     const response = h.response({
       status: 'success',
@@ -37,10 +38,8 @@ const addBookHandler = (request, h) => {
       }
     })
     response.code(201)
-    books.push(newBooks)
     return response
   }
-
   const response = h.response({
     status: 'error',
     message: 'Buku gagal ditambahkan'
@@ -49,11 +48,137 @@ const addBookHandler = (request, h) => {
   return response
 }
 
-const getAllBookHandler = () => ({
-  status: 'success',
-  data: {
-    books
+const getAllBookHandler = (request, h) => {
+  const { name, reading, finished } = request.query
+  if (name) {
+    const book = books.filter((obj) => {
+      return obj.name.toLowerCase().includes(name.toLowerCase())
+    })
+    return {
+      status: 'success',
+      data: {
+        books: book.map(({ id, name, publisher }) => ({ id, name, publisher }))
+      }
+    }
   }
-})
+  if (reading) {
+    const book = books.filter((obj) => {
+      return obj.reading === Boolean(parseInt(reading))
+    })
+    return {
+      status: 'success',
+      data: {
+        books: book.map(({ id, name, publisher }) => ({ id, name, publisher }))
+      }
+    }
+  }
+  if (finished) {
+    const book = books.filter((obj) => {
+      return obj.finished === Boolean(parseInt(finished))
+    })
+    return {
+      status: 'success',
+      data: {
+        books: book.map(({ id, name, publisher }) => ({ id, name, publisher }))
+      }
+    }
+  }
+  return {
+    status: 'success',
+    data: {
+      books: books.map(({ id, name, publisher }) => ({ id, name, publisher }))
+    }
+  }
+}
 
-export { addBookHandler, getAllBookHandler }
+const getBookByIdHandler = (request, h) => {
+  const { id } = request.params
+  const book = books.filter((b) => b.id === id)[0]
+  if (book !== undefined) {
+    return {
+      status: 'success',
+      data: {
+        book
+      }
+    }
+  }
+  const response = h.response({
+    status: 'fail',
+    message: 'Buku tidak ditemukan'
+  })
+  response.code(404)
+  return response
+}
+
+const editBookByIdHandler = (request, h) => {
+  const { id } = request.params
+  const { name, year, author, summary, publisher, pageCount, readPage, reading } = request.payload
+  const updateAt = new Date().toISOString()
+  const finished = readPage === pageCount
+  const index = books.findIndex((b) => b.id === id)
+  if (!name) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal memperbarui buku. Mohon isi nama buku'
+    })
+    response.code(400)
+    return response
+  }
+  if (readPage > pageCount) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal memperbarui buku. readPage tidak boleh lebih besar dari pageCount'
+    })
+    response.code(400)
+    return response
+  }
+  if (index !== -1) {
+    books[index] = {
+      ...books[index],
+      name,
+      year,
+      author,
+      summary,
+      publisher,
+      pageCount,
+      readPage,
+      finished,
+      reading,
+      updateAt
+    }
+    const response = h.response({
+      status: 'success',
+      message: 'Buku berhasil diperbarui'
+    })
+    response.code(200)
+    return response
+  }
+  const response = h.response({
+    status: 'fail',
+    message: 'Gagal memperbarui buku. Id tidak ditemukan'
+  })
+  response.code(404)
+  return response
+}
+
+const deleteBookByIdHandler = (request, h) => {
+  const { id } = request.params
+  const book = books.findIndex((b) => b.id === id)
+  if (book === -1) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Buku gagal dihapus. Id tidak ditemukan'
+    })
+    response.code(404)
+    return response
+  }
+  books.splice(book, 1)
+  const response = h.response({
+    status: 'success',
+    message: 'Buku berhasil dihapus'
+  })
+  response.code(200)
+  return response
+}
+
+export { addBookHandler, getAllBookHandler, getBookByIdHandler, editBookByIdHandler, deleteBookByIdHandler }
--- routes.js
+++ routes.js
@@ -1,4 +1,4 @@
-import { addBookHandler, getAllBookHandler } from './handler.js'
+import { addBookHandler, getAllBookHandler, getBookByIdHandler, editBookByIdHandler, deleteBookByIdHandler } from './handler.js'
 const routes = [
   {
     method: 'POST',
@@ -12,15 +12,44 @@ const routes = [
   },
   {
     method: 'GET',
-    path: '/books',
+    path: '/{books?}',
     handler: getAllBookHandler,
     options: {
       cors: {
         origin: ['*']
       }
     }
+  },
+  {
+    method: 'GET',
+    path: '/books/{id}',
+    handler: getBookByIdHandler,
+    options: {
+      cors: {
+        origin: ['*']
+      }
+    }
+  },
+  {
+    method: 'PUT',
+    path: '/books/{id}',
+    handler: editBookByIdHandler,
+    options: {
+      cors: {
+        origin: ['*']
+      }
+    }
+  },
+  {
+    method: 'DELETE',
+    path: '/books/{id}',
+    handler: deleteBookByIdHandler,
+    options: {
+      cors: {
+        origin: ['*']
+      }
+    }
   }
-
 ]
 
 export default routes
